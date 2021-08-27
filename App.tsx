d24afce73fcab7a3e2118b794a78319f969f3f59
Gonzalo <gon.pineiro@gmail.com>
2021-08-27 08:39:15 -0300

    :hammer: Configuracion de Auth en firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+auth()
+  .createUserWithEmailAndPassword('gon.pineiro@gmail.com', 'SuperSecretPassword!')
+  .then(() => {
+    console.log('User account created & signed in!');
+  })
+  .catch((error) => {
+    if (error.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (error.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(error.code);
+  });
 
 const App = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>();
+
+  // Handle user state changes
+  // eslint-disable-next-line no-shadow
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    setUser(user);
+    if (initializing) {
+      setInitializing(false);
+    }
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  });
+
+  if (initializing) {
+    return null;
+  }
+
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
   return (
     <View>
-      <Text>'asd'</Text>
+      <Text>Welcome {user.email}</Text>
     </View>
   );
 };
