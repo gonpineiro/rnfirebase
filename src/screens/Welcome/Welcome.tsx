9817afd74f0ad88bf9dac8798531519fe1e384bd
Gonzalo <gon.pineiro@gmail.com>
2021-10-22 14:03:47 -0300

    :hammer: Add Welcome Screen

--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { View } from 'react-native';
 
 import { DefaultButton, Separator, Typography } from '../../components';
@@ -8,10 +9,32 @@ import { goToScreen, replaceRoute } from '../../navigation/controls';
 
 // @ts-ignore
 const WelcomeScreen = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  // Handle user state changes
+  const onAuthStateChanged = (user: any) => {
+    setUser(user);
+    if (initializing) {
+      return setInitializing(false);
+    }
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) {
+    return null;
+  }
+  console.log(user);
+
   return (
     <View style={styles.mainContainer}>
       <Typography size={20} variant="medium">
-        Welcome !!!!
+        {user?.email}
       </Typography>
       <Separator size={50} />
       <DefaultButton text="Iniciar " textSize={16} onPress={() => replaceRoute('InicioSesion')} />
